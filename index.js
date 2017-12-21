6fc7fc0fb23fa12aadff4ad785eeaee26c754c75
Andrew Walker <hovercar00@gmail.com>
2017-12-21 04:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts(facts) {
   var newFacts = [];
     var i = 0;
     while (i < facts.lenght) {
-      newFacts.push()
+      newFacts.push(...facts + "!!!");
     }
       
 }
