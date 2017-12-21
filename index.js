eaa7ce880eda5ea40c81234d2367c48d610f07cf
Andrew Walker <hovercar00@gmail.com>
2017-12-21 04:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var facts = {}
   for (i=0; i<(musicians.length); i++) {
-    facts.push(musicians[i] + " plays " + instruments);
+    facts.push(musicians[i] + " plays " + instruments[i]);
   }
 }
